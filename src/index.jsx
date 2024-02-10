510f67a2be2656b110853c3080ad94022b774bdc
Muzietto <marco_faustinelli@yahoo.com>
2024-02-10 20:14:37 +0000

    codesandbox-sortables working perfectly

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '/src/QS-MultipleDraggables/App';
+import App from '/src/codesandbox-sortables/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
